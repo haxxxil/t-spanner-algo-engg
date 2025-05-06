ad753589b3ffc98539728bf5764c59d2d7b809f9
Harshil Singh <harshil.singh@research.iiit.ac.in>
2025-05-06 19:49:26 +0530

    add impl

---
+++ algo/t-spanner.cpp
@@ -0,0 +1,280 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, t;
+
+struct edge
+{
+    int v, w, s, oi;
+    // opposite vertex, weight, status, index in opposite list
+    // status 0 -> removed
+    // status 1 -> possible
+    // status 2 -> in spanner
+};
+
+vector<vector<edge>> adj(0);
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+const int INF = (int)1e9;
+
+bool sample(int n, int k)
+{
+    // 1/n^*(1/k) chance
+    uniform_real_distribution<double> dist(0.0, 1.0);
+    double vl = dist(RNG);
+    double exp = - (1.0/(double)k);
+    double sp = pow(n, exp);
+    return vl<=sp;
+}
+
+void main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>>n>>m>>t; // take parameters
+    adj.resize(n);
+
+    int k = (t+1)/2;
+
+    for(int i=0; i<m; i++)
+    {
+        int u, v, w;
+        cin>>u>>v>>w;
+        edge eu, ev;
+        eu.w = ev.w = w;
+        eu.s = ev.s = 1;
+        eu.oi = adj[v].size();
+        ev.oi = adj[u].size();
+        eu.v = v;
+        ev.v = u;
+        adj[v].push_back(ev);
+        adj[u].push_back(eu);
+    }
+
+    vector<int> cluster(n, 0);
+    vector<int> is_center(n, 0);
+    vector<int> centers(0);
+    vector<int> ivd(n, 1);
+    // fill(ivd.begin(), ivd.end(), 1);
+    iota(cluster.begin(), cluster.end(), 0);
+
+    // phase 1
+    for(int i=1; i<k; i++)
+    {
+        // step 1
+        // sampling
+        fill(is_center.begin(), is_center.end(), 0);
+        int cc = 0;
+        for(auto j: centers)
+        {
+            if(sample(n, k))
+            {
+                is_center[j] = 1;
+                cc++;
+            }
+        }
+        if(!cc) is_center[centers[0]] = 1;
+
+        // step 2
+        vector<int> min_idx(n, -1); 
+        for(int j=0; j<n; j++)
+        {
+            if(!ivd[j]) continue;
+            if(is_center[cluster[j]]) continue;
+            int mw = INF;
+            for(int k=0; k<adj[j].size(); k++)
+            {
+                auto& e = adj[j][k];
+                if(!e.s) continue;
+                if(!is_center[cluster[e.v]]) continue;
+                if(e.w<mw)
+                {
+                    mw = e.w;
+                    min_idx[j] = k;
+                }
+            }
+        }
+
+        //step 3
+        vector<int> center_idx(n, -1);
+        int cidx = 0;
+        for(auto j: centers)
+        {
+            center_idx[j] = cidx;
+            cidx++;
+        }
+        vector<pair<int, int>> to_add(0);
+        vector<pair<int, int>> to_rem(0);
+        vector<pair<int, int>> cluster_change(0);
+        for(int j=0; j<n; j++)
+        {
+            if(!ivd[j]) continue;
+            if(is_center[cluster[j]]) continue;
+            if(min_idx[j]==-1)
+            {
+                vector<int> min_ed(cidx, -1);
+                for(int k=0; k<adj[j].size(); k++)
+                {
+                    auto & e = adj[j][k];
+                    if(e.s!=1) continue;
+                    to_rem.push_back({j, k});
+                    if(center_idx[cluster[e.v]]<0) continue;
+                    int ci = center_idx[cluster[e.v]];
+                    if(min_ed[ci]==-1)
+                    {
+                        min_ed[ci] = k;
+                    }
+                    else if(e.w<adj[j][min_ed[ci]].w)
+                    {
+                        min_ed[ci] = k;
+                    }
+                }
+                ivd[j] = 0;
+                for(int k=0; k<cidx; k++)
+                {
+                    if(min_ed[k]<0) continue;
+                    to_add.push_back({j, min_ed[k]});
+                }
+            }
+        }
+        for(int j=0; j<n; j++)
+        {
+            if(!ivd[j]) continue;
+            if(is_center[cluster[j]]) continue;
+            if(min_idx[j]!=-1)
+            {
+                int ncj = cluster[adj[j][min_idx[j]].v];
+                vector<int> min_ed(cidx, -1);
+                for(int k=0; k<adj[j].size(); k++)
+                {
+                    auto & e = adj[j][k];
+                    if(e.s!=1) continue;
+                    if(!is_center[cluster[e.v]]) to_rem.push_back({j, k});
+                    if(cluster[e.v]==ncj) to_rem.push_back({j, k});
+                    if(center_idx[cluster[e.v]]<0) continue;
+                    int ci = center_idx[cluster[e.v]];
+                    if(min_ed[ci]==-1)
+                    {
+                        min_ed[ci] = k;
+                    }
+                    else if(e.w<adj[j][min_ed[ci]].w)
+                    {
+                        min_ed[ci] = k;
+                    }
+                }
+                cluster_change.push_back({j, ncj});
+                int cw = adj[j][min_idx[j]].w;
+                for(int k=0; k<cidx; k++)
+                {
+                    // if(is_center[centers[k]]) continue;
+                    if(min_ed[k]<0) continue;
+                    if(adj[j][min_ed[k]].w>=cw) continue;
+                    to_add.push_back({j, min_ed[k]});
+                }
+                to_add.push_back({j, min_idx[j]});
+            }
+        }
+        for(auto p: to_add)
+        {
+            auto & e = adj[p.first][p.second];
+            e.s = 2;
+            adj[e.v][e.oi].s = 2;
+        }
+        for(auto p: to_rem)
+        {
+            auto & e = adj[p.first][p.second];
+            if(e.s==2) continue;
+            e.s = 0;
+            adj[e.v][e.oi].s = 0;
+        }
+        for(auto p: cluster_change)
+        {
+            cluster[p.first] = p.second;
+        }
+        
+
+        //step 4
+        for(int j=0; j<n; j++)
+        {
+            if(!is_center[cluster[j]]) continue;
+            for(auto& e: adj[j])
+            {
+                if(cluster[e.v]!=cluster[j]) continue;
+                if(e.s<2) e.s = 0;
+            }
+        }
+
+        centers.clear();
+        for(int j=0; j<n; j++)
+        {
+            if(is_center[j]) centers.push_back(j);
+        }
+    }
+
+
+    //phase 2
+    vector<pair<int, int>> to_add(0);
+    vector<pair<int, int>> to_rem(0);
+
+    vector<int> center_idx(n, -1);
+    int cidx = 0;
+    for(auto i: centers)
+    {
+        center_idx[i] = cidx;
+        cidx++;
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(!ivd[i]) continue;
+        vector<int> min_ed(cidx, -1);
+        for(int j=0; j<adj[i].size(); j++)
+        {
+            auto & e = adj[i][j];
+            if(e.s!=1) continue;
+            if(center_idx[cluster[e.v]]<0) continue;
+            to_rem.push_back({i, j});
+            int ci = center_idx[cluster[e.v]];
+            if(min_ed[ci]==-1)
+            {
+                min_ed[ci] = j;
+            }
+            else
+            {
+                if(e.w<adj[i][min_ed[ci]].w) min_ed[ci] = j;
+            }
+        }
+        for(int j=0; j<cidx; j++)
+        {
+            if(min_ed[j]<0) continue;
+            to_add.push_back({i, min_ed[j]});
+        }
+    }
+
+    for(auto p: to_add)
+    {
+        auto & e = adj[p.first][p.second];
+        e.s = 2;
+        adj[e.v][e.oi].s = 2;
+    }
+    for(auto p: to_rem)
+    {
+        auto & e = adj[p.first][p.second];
+        if(e.s==2) continue;
+        e.s = 0;
+        adj[e.v][e.oi].s = 0;
+    }
+
+    cout<<n<<" "<<m<<"\n";
+    for(int i=0; i<n; i++)
+    {
+        for(auto & e: adj[i])
+        {
+            if(e.v<i) continue;
+            if(e.s<2) continue;
+            cout<<i<<" "<<e.v<<" "<<e.w<<"\n";
+        }
+    }
+}
